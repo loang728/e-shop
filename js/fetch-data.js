a3df01c877fbf51e48103c662dda49ab17be9f89
lora <loang728@gmail.com>
2024-03-09 22:54:00 +0200

    Hovers and design changes

--- js/fetch-data.js
+++ js/fetch-data.js
@@ -1,40 +1,35 @@
- // Fetching data using fetch and async/await
- const fetchData = async () => {
-    try {
-      const response = await fetch("data.json"); // Fetch data from local JSON file
-      const json = await response.json();
-      return json; // Return the entire JSON data from the file
-      console.log(json);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      return [];
-    }
-  };
+// Fetching data using fetch and async/await
+const fetchData = async () => {
+  try {
+    const response = await fetch("data.json"); // Fetch data from local JSON file
+    const json = await response.json();
+    return json; // Return the entire JSON data from the file
+    console.log(json);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+};
 
-  // Function to filter and display cryptocurrency data
-  const displayProductData = async () => {
-    const jsonData = await fetchData();
-    console.log(jsonData);
+// Function to filter and display cryptocurrency data
+const displayProductData = async () => {
+  const jsonData = await fetchData();
+  console.log(jsonData);
 
-    const productsData = jsonData.data;
+  const productsData = jsonData.data;
 
+  //Pagination
+  const ulElement = document.createElement("div");
+  ulElement.classList.add("products");
+  const itemsPerPage = 5;
 
-    //Pagination
-    const ulElement = document.createElement("div");
-    ulElement.classList.add("products");
-    const itemsPerPage = 5;
+  function displayItems(pageNumber) {
+    const startIndex = (pageNumber - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const currentItems = productsData.slice(startIndex, endIndex);
 
-    function displayItems(pageNumber) {
-      const startIndex = (pageNumber - 1) * itemsPerPage;
-      const endIndex = startIndex + itemsPerPage;
-      const currentItems = productsData.slice(startIndex, endIndex);
-  
-     
-    
-
-      
-      const itemsElements = currentItems.map((item) => {
-        return `<article>
+    const itemsElements = currentItems.map((item) => {
+      return `<article>
     <div class="img-section">
       <img src="images/products/Rectangle 518.jpg" alt="placeholder" width="266" height="152">
       <span class="discount">7%</span>
@@ -57,52 +52,33 @@
       <span class="ico add-to-cart cart-white">.</span>
     </div>
   </article>`;
+    });
+
+    ulElement.innerHTML = itemsElements.join("");
+    document.querySelector("div.products-holder").appendChild(ulElement);
+  }
+
+  function displayPaginationButtons(totalPages) {
+    const paginationContainer = document.querySelector(".pagination ul");
+    paginationContainer.innerHTML = "";
+
+    for (let i = 1; i <= totalPages; i++) {
+      const button = document.createElement("li");
+      button.textContent = i;
+      button.addEventListener("click", () => {
+        displayItems(i);
       });
-
-      ulElement.innerHTML = itemsElements.join("");
-      document.querySelector("div.products-holder").appendChild(ulElement);
-  
+      paginationContainer.appendChild(button);
     }
-  
-    function displayPaginationButtons(totalPages) {
-      const paginationContainer = document.querySelector('.pagination ul');
-      paginationContainer.innerHTML = '';
-  
-      for (let i = 1; i <= totalPages; i++) {
-        const button = document.createElement('li');
-        button.textContent = i;
-        button.addEventListener('click', () => {
-          displayItems(i);
-        });
-        paginationContainer.appendChild(button);
-      }
-    }
-  
+  }
 
-    // Initial display
-    displayItems(1);
-    // Calculate total pages
-    const totalPages = Math.ceil(productsData.length / itemsPerPage);
-  
-    // Display pagination buttons
-    displayPaginationButtons(totalPages);
+  // Initial display
+  displayItems(1);
+  // Calculate total pages
+  const totalPages = Math.ceil(productsData.length / itemsPerPage);
 
+  // Display pagination buttons
+  displayPaginationButtons(totalPages);
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  };
-
-  displayProductData();
+displayProductData();
